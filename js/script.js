553dc73e7c96c870b6c3a5f4d333317de1a4982b
Zack <bostianz@gmail.com>
2018-11-14 15:16:14 -0500

    added color clicker heading and an input box, fleshed out features to add in the future

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-var colorPicker = {
+/*
+* Color Clicker
+* Copyright @ Zack Bostian 2018
+/
+* FEATURES TO ADD:
+/
+/
+* Input to specify amount of items to create.
+/    -Limit this amount to 30
+/    -If 0 elements exit function/do nothing
+/    -Input text MUST be a number and MUST be required
+/
+* Button to GENERATE the div items
+/   -Seperate event listener
+/
+/
+*  Move events into own object.
+/
+*/
+
+
+var colorClicker = {
 
   generateHexColor: function() {
     var random;
@@ -12,8 +33,11 @@ var colorPicker = {
   },
 
   renderElements: function() {
+    //var userNumber = document.getElementById().value;
     var divList = document.getElementById('list');
+      // replace 30 with userNumber
       for (var i = 0; i < 30; i++) {
+         // userNumber === 0 ? console.log('true') : console.log('false');
          // Child nodes to divList element
          var divItem = document.createElement('div');
          divItem.classList.add('item');
@@ -34,9 +58,13 @@ var colorPicker = {
         el.style.color = '#fff';
       }
     }.bind(this));
+  },
+
+  clickToRenderElements: function() {
+
   }
 
 }
 
-colorPicker.renderElements();
-colorPicker.setupEventListeners();
+colorClicker.renderElements();
+colorClicker.setupEventListeners();
