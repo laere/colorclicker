58e34cdaedc2a375f5b8e8941200987ce3775b22
Zack <bostianz@gmail.com>
2018-11-15 17:43:33 -0500

    Added ID's to the elements, put event listeners into their own object, displays amount of elements on the web page

--- js/script.js
+++ js/script.js
@@ -4,27 +4,46 @@
 /
 * FEATURES TO ADD:
 /
-/
+* When user clicks button without typing in value
+/ Show pop up bubble that says to enter a value
+/ Can be a css element with an added class of some sort?
+
 *
-/
-/
-/
-/
-*
-/
-/
-/
-*  Move events into own object.
 /
 */
 
+var util = {
+
+  hexColorId: function() {
+    var i, random;
+    var id = '';
+
+    for (i = 0; i < 12; i++) {
+      random = Math.random() * 16 | 0;
+      if (i === 4 || i === 8) {
+        id += '-';
+      }
+      id += random.toString(16);
+    }
+    return id;
+  },
+
+  pluralize: function(count, word) {
+    return count === 1 ? word : word + 's';
+  },
+
+  copyHexColorToClipboard: function() {
+    //When text is selected
+    //copy to clipboard
+  },
+
+};
 
 var colorClicker = {
 
   generateHexColor: function() {
     var random;
     var id = '#';
-
     for (var i = 0; i < 6; i++) {
       random = Math.random() * 16 | 0;
       id += random.toString(16);
@@ -33,23 +52,39 @@ var colorClicker = {
   },
 
   renderElements: function() {
+
     var input = document.getElementById('input');
     var divList = document.getElementById('list');
-    // console.log(input);
-    // console.log(divElementsWithClassItem);
-      // replace 30 with userNumber
+
     for (var i = 0; i < input.value; i++) {
-        if (input.value === 0 || input.value > 30) {
-         return;
-        }
-        var divItem = document.createElement('div');
-        divItem.classList.add('item');
-        divItem.textContent = 'Click me!';
-        divList.appendChild(divItem);
+
+      if (input.value > 30) {
+        return;
+      }
+
+      var divItem = document.createElement('div');
+      divItem.classList.add('item');
+      divItem.textContent = 'Click me!';
+      divItem.setAttribute('id', util.hexColorId());
+      divList.appendChild(divItem);
     }
 
+    input.value = '';
+    input.focus();
     this.hideGenerateButton();
-    // If there's 30 elements on the page hide the generate button.
+    this.showRenderedElementCount();
+    return divList;
+  },
+
+  deleteAllElements: function() {
+    var divElementsWithClassItem = document.getElementsByClassName('item')
+    var nodelistToArray = Array.from(divElementsWithClassItem);
+    console.log(nodelistToArray);
+    var length = nodelistToArray.length;
+    //delete all elements
+    // grab all elements
+    // loop through array and delete the amount of elements based on it's length
+
 
   },
 
@@ -59,46 +94,78 @@ var colorClicker = {
     if (divElementsWithClassItem.length === 30) {
       button.style.visibility = 'hidden';
     }
-
-    // divElementsWithClassItem.length === 30
-    //   ? button.style.visibility = 'hidden'
-    //   : false
-
   },
 
+  showRenderedElementCount: function() {
+    var itemCount = document.getElementById('itemcount');
+    var numberOfElements = document.getElementsByClassName('item');
+    var length = numberOfElements.length;
+    return itemCount.textContent = length + ' ' + util.pluralize(length, 'item');
+  },
+
+};
+
+//   setupEventListeners: function() {
+//     var divList = document.getElementById('list');
+//     var header = document.getElementById('header');
+//     // Mouseover may be less physically intensive?
+//     divList.addEventListener('mouseover', function(e) {
+//       var el = e.target;
+//       if (el.className === 'item') {
+//         el.style.background = this.generateHexColor();
+//         el.textContent = this.generateHexColor();
+//         el.style.color = '#fff';
+//       }
+//     }.bind(this));
+
+//     header.addEventListener('click', function(e) {
+//       var el = e.target;
+//       if (el.id === "btn") {
+//         this.renderElements();
+//       }
+//     }.bind(this));
+//   }
+
+
+var eventListeners = {
+
   setupEventListeners: function() {
+    var ENTER_KEY = 13;
     var divList = document.getElementById('list');
     var header = document.getElementById('header');
     // Mouseover may be less physically intensive?
     divList.addEventListener('mouseover', function(e) {
       var el = e.target;
+      console.log(this);
       if (el.className === 'item') {
-        el.style.background = this.generateHexColor();
-        el.textContent = this.generateHexColor();
+        el.style.background = colorClicker.generateHexColor();
+        el.textContent = colorClicker.generateHexColor();
         el.style.color = '#fff';
       }
-    }.bind(this));
+    });
 
     header.addEventListener('click', function(e) {
       var el = e.target;
       if (el.id === "btn") {
-        this.renderElements();
+        console.log(this);
+        colorClicker.renderElements();
       }
-    }.bind(this));
+    });
 
-  },
+    header.addEventListener('keyup', function(e) {
+      if (e.which === ENTER_KEY) {
+        colorClicker.renderElements();
+      }
+    });
 
-  // clickToRenderElements: function() {
-  //   var button = document.getElementById('btn');
-  //   button.addEventListener('click', this.renderElements);
-  // }
-}
+    header.addEventListener('click', function(e) {
+      var el = e.target;
+      if (el.id === 'btn-delete') {
+        colorClicker.deleteAllElements();
+      }
+    });
 
-// var eventListeners = {
-//
+  }
+};
 
-//
-// }
-
-colorClicker.clickToRenderElements();
-colorClicker.setupEventListeners();
+eventListeners.setupEventListeners();
