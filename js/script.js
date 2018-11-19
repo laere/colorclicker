889a5b37a8a10bd3d3dbb45685d209bad28b8a8e
Zack <bostianz@gmail.com>
2018-11-19 11:15:13 -0500

    Implemented clearall functionality. Also made it so when clearing all elements the element count updates as well.

--- js/script.js
+++ js/script.js
@@ -1,38 +1,46 @@
 /*
-* Color Clicker
+* Color Picker
 * Copyright @ Zack Bostian 2018
-/
-* FEATURES TO ADD:
-/
-* When user clicks button without typing in value
-/ Show pop up bubble that says to enter a value
-/ Can be a css element with an added class of some sort?
-/
-/ Ability to clear all elements.
-*/
-// If user enters a number into the input that makes the total amount of elements > 30, exit the function
 
-//Store elements to local storage
-// retrieve elements in local storage
+// FEATURES TO ADD:
+
+// !! REFACTOR CODE !!
+
+// 1. When user clicks button without typing in value
+//    Show pop up bubble that says to enter a value
+//    Can be a css element with an added class of some sort?
+
+// 2. Set up localstorage
+
+// 3 When you click a hex color make it so the entire viewport
+//    displays that color with the ability to copy the text??
+
+// 4. Add hex color text into middle of div
+//  most likely need to embed a child element into it with the text
+
+// 5. Hide the delete button until hovering oer the div wrapper element
+
+// 6. Style it better, check out how others style their apps, etc.
+
+*/
 
 var util = {
-
   hexColorId: function() {
-      var i, random;
-      var id = '';
+    var i, random;
+    var id = "";
 
-      for (i = 0; i < 12; i++) {
-        random = Math.random() * 16 | 0;
-        if (i === 4 || i === 8) {
-          id += '-';
-        }
-        id += random.toString(16);
+    for (i = 0; i < 12; i++) {
+      random = (Math.random() * 16) | 0;
+      if (i === 4 || i === 8) {
+        id += "-";
       }
+      id += random.toString(16);
+    }
     return id;
   },
 
   pluralize: function(count, word) {
-      return count === 1 ? word : word + 's';
+    return count === 1 ? word : word + "s";
   },
 
   copyHexColorToClipboard: function() {
@@ -48,146 +56,162 @@ var util = {
       return localStorage.getitem(store && JSON.parse(store)) || [];
     }
   }
-
 };
-
 
 var colorClicker = {
   generateHexColor: function() {
     var random;
-    var id = '#';
+    var id = "#";
     for (var i = 0; i < 6; i++) {
-      random = Math.random() * 16 | 0;
+      random = (Math.random() * 16) | 0;
       id += random.toString(16);
     }
     return id;
   },
-
+  // Nodelist doesnt update until after entire render function is finished executing.
   renderElements: function() {
-    var input = document.getElementById('input');
-    var divList = document.getElementById('list');
+    var input = document.getElementById("input");
+    var divList = document.getElementById("list");
+    var nodelist = Array.from(document.querySelectorAll(".item"));
 
     for (var i = 0; i < input.value; i++) {
+      if (input.value > 30) {
+        input.value = "";
+        input.focus();
+        return;
+      }
 
-        var divItem = document.createElement('div');
-        var divCrossout = document.createElement('button');
-        var divWrapper = document.createElement('div');
+      var divItem = document.createElement("div");
+      var divCrossout = document.createElement("button");
+      var divWrapper = document.createElement("div");
 
-        divWrapper.classList.add('item-wrapper')
-        divWrapper.appendChild(divItem)
+      divWrapper.classList.add("item-wrapper");
+      divWrapper.appendChild(divItem);
 
-        divCrossout.classList.add('crossout');
-        divCrossout.textContent = 'X';
-        divCrossout.setAttribute('id', util.hexColorId());
-        divWrapper.appendChild(divCrossout);
+      divCrossout.classList.add("crossout");
+      divCrossout.textContent = "X";
+      divCrossout.setAttribute("id", util.hexColorId());
 
-        divItem.classList.add('item');
-        divItem.textContent = 'Click me!';
-        divList.appendChild(divWrapper);
+      divWrapper.appendChild(divCrossout);
+
+      divItem.classList.add("item");
+      divItem.textContent = "Click me!";
+
+      divList.appendChild(divWrapper);
     }
-
-    input.value = '';
+    input.value = "";
     input.focus();
     this.hideGenerateButton();
     this.showRenderedElementCount();
-    this.render();
-
   },
 
   findElementIndex: function(el) {
-    var nodelist = document.querySelectorAll('.crossout')
+    var nodelist = document.querySelectorAll(".crossout");
     var elementsArray = Array.from(nodelist);
     var id = el.id;
     var i = elementsArray.length;
-    console.log(elementsArray);
 
     while (i--) {
       if (elementsArray[i].id === id) {
-        console.log('this is ' + i);
         return i;
       }
     }
   },
 
   deleteAnElement: function(e) {
-    var nodelist = Array.from(document.querySelectorAll('.item-wrapper'));
-    // Grab index from findElementIndex - refers to index of the buttons which is really the same as the item
-    // However we click the crossout button to get the index of the item position to begin with.
-    // If the index from findElementIndex is equal to the index of the item in the nodelist
-    // Find the parentNode and removeChild the node itself.
+    var nodelist = Array.from(document.querySelectorAll(".item-wrapper"));
+    // Grab index from findElementIndex - refers to index of the buttons
+    // If the index from findElementIndex is equal to the index of the items nodelist
+    // Hide the item
 
     // Turn this into a forEach method
     for (var i = 0; i < nodelist.length; i++) {
       var index = this.findElementIndex(e.target);
-      console.log(index);
       if (i === index) {
         nodelist[i].parentNode.removeChild(nodelist[i]);
-       }
+      }
     }
+    this.showRenderedElementCount();
   },
 
   hideGenerateButton: function() {
-    var divElementsWithClassItem = document.getElementsByClassName('item')
-    var button = document.getElementById('btn');
-    if (divElementsWithClassItem.length === 30) {
-      button.style.visibility = 'hidden';
+    var nodelist = document.querySelectorAll(".item");
+    var button = document.getElementById("btn");
+    if (nodelist.length === 30) {
+      button.style.display = "none";
     }
   },
 
   showRenderedElementCount: function() {
-    var itemCount = document.getElementById('itemcount');
-    var numberOfElements = document.getElementsByClassName('item');
-    var length = numberOfElements.length;
-    return itemCount.textContent = length + ' ' + util.pluralize(length, 'item');
+    var itemCount = document.getElementById("itemcount");
+    var nodelist = document.querySelectorAll(".item-wrapper");
+    var length = nodelist.length;
+    return (itemCount.textContent = length + " " + util.pluralize(length, "item"));
   },
 
   clearAll: function() {
-    console.log('clear all button')
-  },
+    var button = document.getElementById("btn");
+    var divList = document.getElementById("list");
+    var nodelist = Array.from(document.querySelectorAll(".item-wrapper"));
+    for (var i = 0; i < nodelist.length; i++) {
+      nodelist[i].parentNode.removeChild(nodelist[i]);
+    }
+    // Want this to only run if there are elements in the nodelist
+    if (nodelist.length > 0) {
+      this.showRenderedElementCount();
+    }
+    button.style.display = "inline-block";
+  }
 };
 
-
-
 var eventListeners = {
-
   setupEventListeners: function() {
     var ENTER_KEY = 13;
-    var divList = document.getElementById('list');
-    var header = document.getElementById('header');
+    var divList = document.getElementById("list");
+    var header = document.getElementById("header");
     // Mouseover may be less physically intensive?
-    divList.addEventListener('mouseover', function(e) {
+    divList.addEventListener("mouseover", function(e) {
       var el = e.target;
-      if (el.className === 'item') {
+      if (el.className === "item") {
         el.style.background = colorClicker.generateHexColor();
         el.textContent = colorClicker.generateHexColor();
-        el.style.color = '#fff';
+        el.style.color = "#fff";
       }
     });
 
-    divList.addEventListener('click', function(e) {
-      if (e.target.className === 'crossout') {
+       // Deleting individual elements
+    divList.addEventListener("click", function(e) {
+      if (e.target.className === "crossout") {
         colorClicker.deleteAnElement(e);
       }
     });
 
-    header.addEventListener('click', function(e) {
+    // Copy text to clipboard
+    divList.addEventListener('dblclick', function(e) {
+      if (e.target.className === 'item') {
+        console.log('hey');
+      }
+    });
+
+    header.addEventListener("click", function(e) {
       var el = e.target;
       if (el.id === "btn") {
         colorClicker.renderElements();
       }
     });
 
-    header.addEventListener('keyup', function(e) {
+    header.addEventListener("keyup", function(e) {
       if (e.which === ENTER_KEY) {
         colorClicker.renderElements();
       }
     });
 
-    header.addEventListener('click', function(e) {
-      if (e.target.id === 'btn-delete') {
+    header.addEventListener("click", function(e) {
+      if (e.target.id === "btn-delete") {
         colorClicker.clearAll();
       }
     });
+
 
   }
 };
