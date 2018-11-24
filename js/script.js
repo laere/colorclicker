536559d2ce517d7733b8115d03b8034bf19375cb
Zack <bostianz@gmail.com>
2018-11-24 16:33:06 -0500

    Added ability to click item colors and display the entire viewport with the color and hex number, and also able to click anywhere to exit the current color in the viewport

--- js/script.js
+++ js/script.js
@@ -6,21 +6,14 @@
 
 // !! REFACTOR CODE !!
 
-// 1. When user clicks button without typing in value
-//    Show pop up bubble that says to enter a value
-//    Can be a css element with an added class of some sort?
-
 // 2. Set up localstorage
-
-// 3 When you click a hex color make it so the entire viewport
-//    displays that color with the ability to copy the text??
 
 // 4. Add hex color text into middle of div
 //  most likely need to embed a child element into it with the text
 
 // 5. Hide the delete button until hovering oer the div wrapper element
 
-// 6. Style it better, check out how others style their apps, etc.
+// 6. Option to change hex value to rgb values.
 
 */
 
@@ -50,12 +43,17 @@ var util = {
   // localstorage
   store: function(namespace, data) {
     if (arguments.length > 1) {
-      return localStorage.setItem(namespace, JSON.stringify(data));
+      return localStorage.setItem(namespace, JSON.stringify(data, ['className', 'tagName', 'childNodes']));
     } else {
-      var stor = localStorage.getItem(namespace);
-      return localStorage.getitem(store && JSON.parse(store)) || [];
+      var store = localStorage.getItem(JSON.parse(namespace))
+      return (store && JSON.stringify(store)) || [];
     }
-  }
+  },
+
+  saveElements: function() {
+    // var nodelist = Array.from(document.querySelectorAll('.item-wrapper'));
+    // this.store('colors', nodelist);
+  },
 };
 
 var colorClicker = {
@@ -73,6 +71,16 @@ var colorClicker = {
     var input = document.getElementById("input");
     var divList = document.getElementById("list");
     var nodelist = Array.from(document.querySelectorAll(".item"));
+    var popup = document.querySelector('.popup');
+
+    // Add an animation to it?
+    // Empty input or 0.
+    if (input.value === '') {
+      popup.style.display = 'block';
+    } else {
+      popup.style.display = 'none';
+    }
+
 
     for (var i = 0; i < input.value; i++) {
       if (input.value > 30) {
@@ -81,9 +89,11 @@ var colorClicker = {
         return;
       }
 
+
       var divItem = document.createElement("div");
       var divCrossout = document.createElement("button");
       var divWrapper = document.createElement("div");
+
 
       divWrapper.classList.add("item-wrapper");
       divWrapper.appendChild(divItem);
@@ -96,6 +106,9 @@ var colorClicker = {
 
       divItem.classList.add("item");
       divItem.textContent = "Click me!";
+      divItem.setAttribute('id', util.hexColorId());
+      divItem.setAttribute('toggle', false);
+
 
       divList.appendChild(divWrapper);
     }
@@ -104,7 +117,8 @@ var colorClicker = {
     this.hideGenerateButton();
     this.showRenderedElementCount();
   },
-
+  // finds index in an array of elements based on ID
+  // Pass in a nodelist arg if reusing.
   findElementIndex: function(el) {
     var nodelist = document.querySelectorAll(".crossout");
     var elementsArray = Array.from(nodelist);
@@ -118,13 +132,15 @@ var colorClicker = {
     }
   },
 
+
+
   deleteAnElement: function(e) {
     var nodelist = Array.from(document.querySelectorAll(".item-wrapper"));
     // Grab index from findElementIndex - refers to index of the buttons
     // If the index from findElementIndex is equal to the index of the items nodelist
     // Hide the item
 
-    // Turn this into a forEach method
+    // Turn this into a forEach method?
     for (var i = 0; i < nodelist.length; i++) {
       var index = this.findElementIndex(e.target);
       if (i === index) {
@@ -146,7 +162,8 @@ var colorClicker = {
     var itemCount = document.getElementById("itemcount");
     var nodelist = document.querySelectorAll(".item-wrapper");
     var length = nodelist.length;
-    return (itemCount.textContent = length + " " + util.pluralize(length, "item"));
+    return (itemCount.textContent =
+      length + " " + util.pluralize(length, "item"));
   },
 
   clearAll: function() {
@@ -161,16 +178,44 @@ var colorClicker = {
       this.showRenderedElementCount();
     }
     button.style.display = "inline-block";
-  }
+  },
+
+  expandedColor: function(e) {
+      var container = document.getElementById('container');
+      var expandedColor = document.getElementById('expanded-color');
+      var expandedColorText = document.getElementById('expanded-color__text');
+      var exitMessage = document.getElementById('exitmessage');
+      var el = e.target;
+      var colorClicked = el.style.background;
+      var hexText = el.textContent;
+
+
+      container.style.display = 'none'
+      expandedColor.style.display = 'block';
+      expandedColor.style.background = colorClicked;
+      expandedColorText.textContent = hexText;
+
+    },
+
+
+  exitExpandedColor: function(e) {
+      var container = document.getElementById('container');
+      var expandedColor = document.getElementById('expanded-color');
+      var expandedColorText = document.getElementById('expanded-color__text');
+      var exitMessage = document.getElementById('exitmessage');
+
+      container.style.display = ''
+      expandedColor.style.display = 'none';
+
+    }
 };
 
 var eventListeners = {
   setupEventListeners: function() {
     var ENTER_KEY = 13;
-    var divList = document.getElementById("list");
-    var header = document.getElementById("header");
+    var container = document.getElementById('container');
     // Mouseover may be less physically intensive?
-    divList.addEventListener("mouseover", function(e) {
+    container.addEventListener("mouseover", function(e) {
       var el = e.target;
       if (el.className === "item") {
         el.style.background = colorClicker.generateHexColor();
@@ -179,39 +224,56 @@ var eventListeners = {
       }
     });
 
-       // Deleting individual elements
-    divList.addEventListener("click", function(e) {
+    // Deleting individual elements
+    container.addEventListener("click", function(e) {
       if (e.target.className === "crossout") {
         colorClicker.deleteAnElement(e);
       }
     });
 
     // Copy text to clipboard
-    divList.addEventListener('dblclick', function(e) {
+    // divList.addEventListener('dblclick', function(e) {
+    //   if (e.target.className === 'item') {
+    //     console.log('hey');
+    //   }
+    // });
+
+    container.addEventListener('click', function(e) {
       if (e.target.className === 'item') {
-        console.log('hey');
+        colorClicker.expandedColor(e);
       }
     });
 
-    header.addEventListener("click", function(e) {
+    container.addEventListener("click", function(e) {
       var el = e.target;
       if (el.id === "btn") {
         colorClicker.renderElements();
       }
     });
 
-    header.addEventListener("keyup", function(e) {
+    container.addEventListener("keyup", function(e) {
       if (e.which === ENTER_KEY) {
         colorClicker.renderElements();
       }
     });
 
-    header.addEventListener("click", function(e) {
+    container.addEventListener("click", function(e) {
       if (e.target.id === "btn-delete") {
         colorClicker.clearAll();
       }
     });
 
+    // container.addEventListener('click', function(e) {
+    //   if (e.target.id === 'btn__save') {
+    //     util.saveElements();
+    //   }
+    // });
+
+    document.body.addEventListener('click', function(e) {
+        if (e.target.id === 'expanded-color') {
+          colorClicker.exitExpandedColor();
+        }
+      });
 
   }
 };
